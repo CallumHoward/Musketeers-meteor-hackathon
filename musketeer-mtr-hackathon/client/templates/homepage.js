1f7cda1dce3a417a8ac6c9be33172f887d1402a2
CallumHoward <callumh@cse.unsw.edu.au>
2015-10-11 14:09:25 +1100

    select canvas from homepage

--- musketeer-mtr-hackathon/client/templates/homepage.js
+++ musketeer-mtr-hackathon/client/templates/homepage.js
@@ -1,3 +1,38 @@
 Template.homepage.rendered = function () {
   $( ".draggable" ).draggable();
 };
+
+Template.homepage.events({
+    "click .canvas-selector": function (e) {
+        var canvasId = $(e.target).data("id");
+        var elementIds = Canvases.findOne(canvasId).elements;
+        Session.set("currentCanvas", elementIds);
+        console.log("clicked");
+    }
+});
+
+Template.homepage.helpers({
+    // given canvas id, returns all elements in canvas
+    currentCanvasHelper: function() {
+        var elementIds = Session.get("currentCanvas");
+        return _.map(elementIds, function(elementId){
+            return Elements.findOne(elementId);
+        });
+    },
+
+    getCurrentCanvasName: function() {
+        var canvasId = Session.get("currentCanvasId");
+        if (canvasId) {
+            return Canvases.findOne(canvasId).name;
+        } else {
+            return "untitled_canvas";
+        }
+    },
+    isText: function() {
+      return this.type === 'text';
+    },
+    isImage: function() {
+      return this.type === 'image';
+    }
+});
+
